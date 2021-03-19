7e34bcecd240ba657e500b565deb8a142ba261d6
juliet879 <gisembajuliett@gmail.com>
2021-03-19 09:37:47 +0300

    add complex function

--- src/Arrays.kt
+++ src/Arrays.kt
@@ -3,6 +3,7 @@ import java.util.*
 fun main() {
     continents("Africa","Asia","Europe","North America")
     capitals()
+    complex()
 
 }
 fun continents(cont1:String,cont2:String,cont3:String,cont4:String) {
@@ -18,3 +19,11 @@ fun capitals() {
     }
 }
 
+fun complex() {
+    var numbers = arrayOf(32,17,4,213,78,43,90,31,3,73,11,158,62)
+
+    var add = numbers[1].plus(numbers[5])
+    println(add)
+    println(numbers.indexOf(158))
+    println(Arrays.toString(numbers.sortedArray()))
+}
