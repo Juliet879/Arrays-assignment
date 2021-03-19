c1254f88d47cd0b28bffde14390ae2239d6b0220
juliet879 <gisembajuliett@gmail.com>
2021-03-19 09:24:27 +0300

    add capitals function

--- src/Arrays.kt
+++ src/Arrays.kt
@@ -2,9 +2,19 @@ import java.util.*
 
 fun main() {
     continents("Africa","Asia","Europe","North America")
+    capitals()
 
 }
 fun continents(cont1:String,cont2:String,cont3:String,cont4:String) {
     var myArrays = arrayOf(cont1,cont2,cont3,cont4)
      println(Arrays.toString(myArrays))
 }
+
+fun capitals() {
+    var cities = arrayOf("harare","mumbai","dodoma","jakarta")
+
+    cities.forEach { city->
+        println(city.capitalize())
+    }
+}
+
