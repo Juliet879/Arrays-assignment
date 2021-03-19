5d12f435ad1c0f803a528e47f7ab740dc38ca49d
juliet879 <gisembajuliett@gmail.com>
2021-03-19 09:13:31 +0300

    add first function

---
+++ src/Arrays.kt
@@ -0,0 +1,10 @@
+import java.util.*
+
+fun main() {
+    continents("Africa","Asia","Europe","North America")
+
+}
+fun continents(cont1:String,cont2:String,cont3:String,cont4:String) {
+    var myArrays = arrayOf(cont1,cont2,cont3,cont4)
+     println(Arrays.toString(myArrays))
+}
